aa1b236c9269d9fb002feca34c98441abb9f4464
davissears <davisrandsears@gmail.com>
2026-02-03 19:45:17 -0800

    Refactor Jot model to use setter for tier
    Introduces a setter for the `tier` property in the `Jot` model. This
    setter includes validation to ensure only allowed tier values are
    assigned, throwing an error for invalid inputs.

--- src/models/todo.js
+++ src/models/todo.js
@@ -2,21 +2,40 @@ export class Jot {
   #id = crypto.randomUUID();
   #tier = "";
   constructor(tier, title) {
+    this.tier = tier;
     this.title = title;
+    // const validTiers = ["PROJECT", "TODO", "CHECKLIST", "CHECKITEM"];
+    // if (validTiers.includes(this.tier)) {
+    //   return (this.tier = tier);
+    // } else {
+    //   throw new Error(`invalid jot tier value:${tier}`);
+    // }
   }
   get id() {
     return this.#id;
   }
 
+  get tier() {
+    return this.#tier;
+  }
+  set tier(tier) {
+    const validTiers = ["PROJECT", "TODO", "CHECKLIST", "CHECKITEM"];
+    if (validTiers.includes(tier)) {
+      this.#tier = tier;
+    } else {
+      throw new Error(`invalid jot tier value:${tier}`);
+    }
+  }
   // get priority() {
   //   return this.priority;
   // }
 }
 // NOTE: testing
 // object should be extensible to all other like objects
-const sampleItem = new Jot("sample", "Walk dog");
-console.log("new jot", sampleItem);
-// test:pass
-// sampleItem.id = "hello";
-// console.log('id expected to not be "hello" ', sampleItem, sampleItem.id);
-sampleItem.tier = "project";
+const sampleItem = new Jot("TODO", "Walk dog");
+console.log(`tier expected to be: "TODO"`, sampleItem, sampleItem.tier);
+sampleItem.tier = "PROJECT";
+console.log(`tier expected to be "PROJECT"`, sampleItem, sampleItem.tier);
+// sampleItem.tier = "coconut"; //this should throw
+// sampleItem.tier = "project"; // this should throw
+console.log('tier expected to be "PROJECT" ', sampleItem, sampleItem.tier);
