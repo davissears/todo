b44d2448c6858b4ac94373302912f46380efa0a4
davissears <davisrandsears@gmail.com>
2026-02-06 16:22:20 -0800

    FIX: setters using return Remove explicit return from setters.
    The previous implementation of setters for `note` and `priority` in the
    `Project` class included explicit `return` statements. These are
    unnecessary as setters are not expected to return values. This change
    yeilds clean & readable methods with expected behavior

--- src/models/project.js
+++ src/models/project.js
@@ -2,30 +2,45 @@ import Jot from "./jot";
 import Priority from "./priority";
 import Note from "./note";
 
+// TODO: remove `return` statements from setters
+
 export default class Project extends Jot {
   #note;
+  #priority;
   constructor(title) {
     super("PROJECT", title);
     this.tier = "PROJECT";
   }
 
-  setNote(value) {
-    if (this.#note) {
-      return (this.#note.note = value);
-    } else if (!this.note) {
-      return (this.#note = new Note(value, this.groupId));
-    }
-  }
-
   get note() {
-    return this.#note;
+    //   this.#note= object.this.#note.note= objectsnote` prop
+    return this.#note.note;
   }
 
   set note(value) {
-    return this.setNote(value);
+    this.#note = new Note(value);
   }
 
-  setPriority(value) {
-    return (this.priority = new Priority(value));
+  get priority() {
+    return this.#priority.priority;
+  }
+
+  set priority(value) {
+    this.#priority = new Priority(value);
   }
 }
+
+const sample = new Project("Make a Project");
+console.log(sample, sample.note);
+sample.note = "HIGH";
+console.log(
+  "!!!init sample.priority object, use setter & getter:::HIGH:::!!!",
+  sample,
+  sample.note,
+); // ! PASS
+sample.note = "LOW";
+console.log(
+  "!!!overwrite priority value with set:::LOW:::",
+  sample,
+  sample.note,
+);
