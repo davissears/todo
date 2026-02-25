1b0b694c5124bb7acaab7f0a3e7534f9e3cdec5d
davissears <davisrandsears@gmail.com>
2026-02-24 18:51:23 -0800

    Add serialization for Todo objects
    This commit introduces the `serializeTodo` method to the
    `StorageService` class, enabling the conversion of `Todo` objects into a
    serializable format.

--- src/services/storage.js
+++ src/services/storage.js
@@ -3,6 +3,7 @@ import Note from "../model/objects/properties/note.js";
 import Project from "../model/objects/project.js";
 import CheckItem from "../model/objects/checkItem.js";
 import Checklist from "../model/objects/checklist.js";
+import Todo from "../model/objects/todo.js";
 
 // NOTE: new storage class goes here
 export default class StorageService {
@@ -131,6 +132,28 @@ export default class StorageService {
     return instance;
   }
 
+  serializeTodo(obj) {
+    if (obj !== undefined) {
+      return {
+        items: obj.items.map((item) => {
+          if (item.tier === "CHECKLIST") return this.serializeChecklist(item);
+          return item;
+        }),
+        note: this.serializeNoteObj(obj.note),
+        note: this.serializeNoteObj(obj.note),
+        priority: this.serializePriorityObj(obj.priority),
+        title: obj.title,
+        tier: obj.tier,
+        id: obj.id,
+        groupId: obj.groupId,
+        description: obj.description,
+        status: obj.status,
+        dueDateTime: this.serializeDateObj(obj.dueDateTime),
+      };
+    }
+  }
+
+  deserializeTodo(storedObj) {}
   // ?: does it make sense to write a method to call methods for objects in `items`?
   // NOTE: PRO: removes multiple lines of code
   // NOTE: CON: adds complexity & dependancy on other function
@@ -179,28 +202,36 @@ export default class StorageService {
 const storage = new StorageService();
 // const project = new Project("!!! PROJECT !!!");
 // const original = new CheckItem("!!!SAMPLE CHECK ITEM!!!");
-const original = new Checklist("!!!SAMPLE CHECKLIST!!!");
-const originalItem = new CheckItem("!!!SAMPLE CHECKITEM!!!");
-original.items = [originalItem];
+const original = new Todo("!!!SAMPLE TODO!!!");
+const originalCL = new Checklist("!!!SAMPLE CHECEKLIST!!!");
+originalCL.description = "is a checklist";
+originalCL.status = "ACTIVE";
+originalCL.dueDateTime = `2027-02-20T22:07:50.128Z`;
+originalCL.priority = "EMERGENCY";  
+original.items = [originalCL];
+const item1 = new CheckItem("item1");
+const item2 = new CheckItem("item2");
+const item3 = new CheckItem("item3");
+originalCL.items = [item1, item2, item3];
 original.note = "Critical metadata about the task";
 original.priority = "EMERGENCY";
 original.description = "::: DESCRIPTION :::";
 original.status = "ACTIVE";
 original.dueDateTime = `2027-02-20T22:07:50.128Z`;
-console.log(":::CHECKLIST:::", original);
+console.log(":::PARENT:::", original);
 // 2. Perform the Round Trip
-const serialized = storage.serializeChecklist(original);
+const serialized = storage.serializeTodo(original);
 console.log(
-  ":::SERIALIZED:::CHECKLIST:::",
+  ":::SERIALIZED:::PARENT:::",
   serialized,
-  ":::SERIALIZED:::ITEMS:::",
+  ":::SERIALIZED:::CHILD:::",
   serialized.items,
 );
-const rehydrated = storage.deserializeChecklist(serialized);
+const rehydrated = storage.deserializeTodo(serialized);
 console.log(
-  ":::REHYDRATED:::CHECKLIST:::",
+  ":::REHYDRATED:::PARENT:::",
   rehydrated,
-  ":::REHYDRATED:::ITEMS:::",
+  ":::REHYDRATED:::CHILD:::",
   rehydrated.items,
 );
 
@@ -229,5 +260,10 @@ verify("tier match", original.tier === rehydrated.tier);
 verify(
   "children match",
   original.items.length === rehydrated.items.length &&
+    original.items[0].id === rehydrated.items[0].id,
+);
+verify(
+  "grand-children match",
+  originalCL.items.length === rehydrated.items.length &&
     original.items[0].id === rehydrated.items[0].id,
 );
