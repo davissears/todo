855f7c2f73401cab876704f3fe58d81de31501e4
davissears <davisrandsears@gmail.com>
2026-03-29 00:45:41 -0700

    Build: Copy worker script to dist
    Copies the _worker.js file to the dist directory during the build
    process.

--- build.ts
+++ build.ts
@@ -1,6 +1,10 @@
+import { copyFileSync } from "fs";
+
 await Bun.build({
 	entrypoints: ["./index.html"],
 	outdir: "./dist",
 	minify: true,
 	sourcemap: "inline",
 });
+
+copyFileSync("_worker.js", "dist/_worker.js");
